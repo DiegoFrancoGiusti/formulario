cdef6d2647d5c71dc69a4276d8b747c23bca08f8
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-05-12 20:32:11 -0300

    validando os inputs

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('.c-form')
 const messageContent = document.querySelectorAll('small')
+const passwordRrequiredsItems = document.querySelectorAll('.c-required__item')
+const button = document.querySelector('.c-form__button')
+const z = document.createElement('p')
 let regexResult = null
+let x = null
+const voidRegex = /[ ]/
 const inputs = [
   form.inputName,
   form.inputEmail,
@@ -22,13 +27,28 @@ const hiddenMessageError = (input,index) => {
 }
 
 inputs[4].addEventListener('input', () => {
+  const lowerCaseRegex = /(?=.*[a-z])/
+  const upperCaseRegex = /(?=.*[A-Z])/
+  const numberRegex = /(?=.*[0-9])/
+  const symbolRegex = /(?=.*[!@#$%&~^()])/
   let passwordInputValue = inputs[4].value
-  
+  const expressions = [
+    !voidRegex.test(passwordInputValue),
+    passwordInputValue.length >= 6,
+    lowerCaseRegex.test(passwordInputValue),
+    upperCaseRegex.test(passwordInputValue),
+    numberRegex.test(passwordInputValue),
+    symbolRegex.test(passwordInputValue)
+  ]
 
-  //console.log(passwordRegex.test(passwordInputValue))
-  for (const match of passwordInputValue.matchAll(passwordRegex)) {
-    console.log(match.groups.upperCase)
-  }
+  expressions[0] ? hiddenMessageError(inputs[4],4) : showMessageError(inputs[4],4,'A senha não pode conter espaços em branco')
+  expressions[1] ? passwordRrequiredsItems[0].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[0].style.listStyleImage = 'url(../../img/incorrect_icon.png)' 
+  expressions[2] ? passwordRrequiredsItems[1].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[1].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  expressions[3] ? passwordRrequiredsItems[2].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[2].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  expressions[4] ? passwordRrequiredsItems[3].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[3].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  expressions[5] ? passwordRrequiredsItems[4].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[4].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+
+  x = expressions
 })
 
 form.addEventListener('submit', event => {
@@ -37,6 +57,11 @@ form.addEventListener('submit', event => {
   inputs.forEach((input,index) => {
     if(input.value === ''){
       showMessageError(input,index,'Campo obrigatório')
+      return
+    }
+
+    if(voidRegex.test(input.value)){
+      showMessageError(input,index,'Valor do campo inválido')
       return
     }
 
@@ -59,15 +84,38 @@ form.addEventListener('submit', event => {
         return
       }
     }
+
+    if(input === inputs[2]){
+      const telRegex = /(\(?\d{2}\)?\s)?[ ]?(\d{4,5}\-?\d{4})/g
+      const x = telRegex.test(input.value)
+
+      if(x === false){
+        showMessageError(input,index,'Telefone inválido')
+        return
+      }
+    }
+
+    if(input === inputs[4]){
+      if(x.some(item => item === false)){
+        showMessageError(input,index,'Senha inválida')
+        return
+      }
+    }
+
+    if(input === inputs[5]){
+      if(input.value !== inputs[4].value){
+        showMessageError(input,index,'Senha inválida')
+        return
+      }
+    }
+
     hiddenMessageError(input,index)
+    z.innerText = 'Cadastro realizado!'
+    z.classList.add('message-success')
+    button.insertAdjacentElement('afterend',z)
+
   })
+  
 })
 
-/*const personList = `First_Name: John, Last_Name: Doe
-First_Name: Jane, Last_Name: Smith`;
 
-const regexpNames =
-  /First_Name: (?<firstname>\w+), Last_Name: (?<lastname>\w+)/gm;
-for (const match of personList.matchAll(regexpNames)) {
-  console.log(`Hello ${match.groups.firstname} ${match.groups.lastname}`);
-}*/
