aa8a6bd40f39b54007607cc15547de609aa69e0f
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-05-18 19:42:40 -0300

    refatorando o código

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const form = document.querySelector('.c-form')
 const messageContent = document.querySelectorAll('small')
 const passwordRrequiredsItems = document.querySelectorAll('.c-required__item')
 const button = document.querySelector('.c-form__button')
-const z = document.createElement('p')
+const paragraph = document.createElement('p')
 let regexResult = null
-let x = null
+let expressionsResults = null
 const voidRegex = /[ ]/
 const inputs = [
   form.inputName,
@@ -26,6 +26,93 @@ const hiddenMessageError = (input,index) => {
   messageContent[index].classList.remove('message-error')
 }
 
+const isNameValid = name => {
+  const regexName = /^[a-záãéêíóõçñ ]+$/i
+  regexResult = regexName.test(name.value)
+
+  if(regexResult) {
+    hiddenMessageError(name,0)
+    return true
+  }
+
+  return false
+}
+
+const isEmailValid = email => {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+  regexResult = emailRegex.test(email.value)
+
+  if(regexResult){
+    hiddenMessageError(email,1)
+    return true
+  }
+
+  return false
+}
+
+const isPhoneValid = phone => {
+  const phoneRegex = /(\(?\d{2}\)?\s)?[ ]?(\d{4,5}\-?\d{4})/g
+  regexResult = phoneRegex.test(phone.value)
+
+  if(regexResult){
+    hiddenMessageError(phone,2)
+    return true
+  }
+
+  return false
+}
+
+const isPasswordvalid = password => {
+  const x = expressionsResults.some(item => item === false)
+
+  if(!x){
+    hiddenMessageError(password,2)
+    return true
+  }
+
+ return false
+}
+
+const isPasswordSame = repeatpassword => {
+
+  if(repeatpassword.value === inputs[4].value){
+    hiddenMessageError(repeatpassword,5)
+    return true
+  }
+
+  return false
+}
+
+const successSubmit = () => {
+  paragraph.innerText = 'Cadastro realizado!'
+  paragraph.classList.add('message-success')
+  button.insertAdjacentElement('afterend',paragraph)
+}
+
+const hiddenSuccessSubmit = () => {
+  setTimeout(() => {
+    paragraph.remove()
+  },2000)
+}
+
+const resetValues = () => {
+  inputs.forEach(input => {
+    input.value = ''
+  })
+
+  passwordRrequiredsItems.forEach(requiredItem => {
+    requiredItem.style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  })
+
+  expressionsResults = null
+}
+
+const setIconsPasswordRequired = expressions => {
+  for(let i = 1;i <= 5;i++){
+    expressions[i] ? passwordRrequiredsItems[i-1].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[i-1].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  }
+}
+
 inputs[4].addEventListener('input', () => {
   const lowerCaseRegex = /(?=.*[a-z])/
   const upperCaseRegex = /(?=.*[A-Z])/
@@ -42,13 +129,9 @@ inputs[4].addEventListener('input', () => {
   ]
 
   expressions[0] ? hiddenMessageError(inputs[4],4) : showMessageError(inputs[4],4,'A senha não pode conter espaços em branco')
-  expressions[1] ? passwordRrequiredsItems[0].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[0].style.listStyleImage = 'url(../../img/incorrect_icon.png)' 
-  expressions[2] ? passwordRrequiredsItems[1].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[1].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
-  expressions[3] ? passwordRrequiredsItems[2].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[2].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
-  expressions[4] ? passwordRrequiredsItems[3].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[3].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
-  expressions[5] ? passwordRrequiredsItems[4].style.listStyleImage = 'url(../../img/correct_icon.png)' : passwordRrequiredsItems[4].style.listStyleImage = 'url(../../img/incorrect_icon.png)'
+  setIconsPasswordRequired(expressions)
 
-  x = expressions
+  expressionsResults = expressions
 })
 
 form.addEventListener('submit', event => {
@@ -66,76 +149,32 @@ form.addEventListener('submit', event => {
     }
   })
 
-  if(inputs[0]){
-    const regexName = /[a-záãéêíóõçñ]+$/i
-    regexResult = regexName.test(inputs[0].value)
-    
-    if(regexResult === false){
-      showMessageError(inputs[0],0,'Nome inválido')
-      return
-    }
-    hiddenMessageError(inputs[0],0)
+  if(!isNameValid(inputs[0])){
+    showMessageError(inputs[0],0,'Nome inválido')
+    return
   }
 
-  if(inputs[1]){
-    const regexEmail = /\S+@\S+\.\S+/
-    regexResult = regexEmail.test(inputs[1].value)
-
-    if(regexResult === false){
-      showMessageError(inputs[1],1,'Email inválido')
-      return
-    }
-
-    hiddenMessageError(inputs[1],1)
+  if(!isEmailValid(inputs[1])){
+    showMessageError(inputs[1],1,'Email inválido')
+    return
   }
 
-  if(inputs[2]){
-    const telRegex = /(\(?\d{2}\)?\s)?[ ]?(\d{4,5}\-?\d{4})/g
-    regexResult = telRegex.test(inputs[2].value)
-
-    if(regexResult === false){
-      showMessageError(inputs[2],2,'Telefone inválido')
-      return
-    }
-
-    hiddenMessageError(inputs[2],2)
+  if(!isPhoneValid(inputs[2])){
+    showMessageError(inputs[2],2,'Telefone inválido')
+    return
+  }
+  
+  if(!isPasswordvalid(inputs[4])){
+    showMessageError(inputs[4],4,'Senha inválida')
+    return
   }
 
-  if(inputs[4]){
-    if(x.some(item => item === false)){
-      showMessageError(inputs[4],4,'Senha inválida')
-      return
-    }
-
-    hiddenMessageError(inputs[4],4)
+  if(!isPasswordSame(inputs[5])){
+    showMessageError(inputs[5],5,'Senha inválida')
+    return
   }
 
-  if(inputs[5]){
-    if(inputs[5].value !== inputs[4].value){
-      showMessageError(inputs[5],5,'Senha inválida')
-      return
-    }
-
-    hiddenMessageError(inputs[5],5)
-  }
-
-    z.innerText = 'Cadastro realizado!'
-    z.classList.add('message-success')
-    button.insertAdjacentElement('afterend',z)
-
-  inputs.forEach(input => {
-    input.value = ''
-  })
-
-  x = null
-
-  passwordRrequiredsItems.forEach(requiredItem => {
-    requiredItem.style.listStyleImage = 'url(../../img/incorrect_icon.png)'
-  })
-
-  setTimeout(() => {
-    z.remove()
-  },2000)
+  successSubmit()
+  resetValues()
+  hiddenSuccessSubmit()
 })
-
-
